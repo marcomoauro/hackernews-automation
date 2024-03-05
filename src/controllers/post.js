e20a617e7017c20fe5e7dd5980d2a2e7cc74425f
Marco Moauro <marco.moauro@translated.net>
2024-03-05 23:14:14 +0100

    remove querystring

--- src/controllers/post.js
+++ src/controllers/post.js
@@ -4,6 +4,8 @@ import Post from "../models/Post.js";
 export const getPostUrlByPath = async ({path}) => {
   log.info('Controller::Post::getPostUrlByPath', {path})
 
+  path &&= path.split('?').at(0);
+
   const post = await Post.getByPath(path)
 
   return {url: post.url}
